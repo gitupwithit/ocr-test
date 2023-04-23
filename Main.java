08cc5f8d389ecb7fc9cd3913abe1bf68447ceb86
RewindCat <8024323-RewindCat@users.noreply.replit.com>
2023-04-23 04:05:37 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,94 @@
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.awt.event.InputEvent;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
+
+public class Main {
+
+  public static List<String> readBarcodesFromFile(String filename) throws IOException {
+    List<String> barcodes = new ArrayList<>();
+    BufferedReader reader = new BufferedReader(new FileReader(filename));
+    String line;
+    while ((line = reader.readLine()) != null) {
+      barcodes.add(line.trim());
+    }
+    reader.close();
+    return barcodes;
+  }
+
+  public static void enterBarcode(Robot robot, String barcode) {
+    // Locate the input field in Symphony Workflows
+    // Click the input field
+    // Type the barcode using robot.keyPress() and robot.keyRelease()
+  }
+
+  public static void saveInfoToFile(String info, String outputFilename) throws IOException {
+    FileWriter writer = new FileWriter(outputFilename, true);
+    writer.write(info + "\n");
+    writer.close();
+  }
+
+  public static String readInfo(String imagePath) throws TesseractException {
+    Tesseract tesseract = new Tesseract();
+    tesseract.setDatapath("path/to/tessdata");
+    BufferedImage image = null;
+
+    try {
+      image = ImageIO.read(new File(imagePath));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    String result = tesseract.doOCR(image);
+    return result;
+  }
+
+
+  public static void main(String[] args) {
+    String inputFilename = "barcodes.txt";
+    String outputFilename = "output.txt";
+
+    try {
+      Robot robot = new Robot();
+      List<String> barcodes = readBarcodesFromFile(inputFilename);
+
+      // Define the Rectangle for capturing the screen area containing the info
+      int x = 0;
+      int y = 0;
+      int width = 640;
+      int height = 320;
+
+      Rectangle infoRect = new Rectangle(x, y, width, height);
+
+      for (String barcode : barcodes) {
+          enterBarcode(robot, barcode);
+          try {
+              Thread.sleep(1000); // Wait for the application to process the barcode
+          } catch (InterruptedException e) {
+              e.printStackTrace();
+          }
+
+          BufferedImage image = robot.createScreenCapture(infoRect);
+          String imagePath = "captured_image.png";
+          ImageIO.write(image, "png", new File(imagePath));
+
+          String info = readInfo(imagePath);
+          saveInfoToFile(info, outputFilename);
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+  }
+
+}
